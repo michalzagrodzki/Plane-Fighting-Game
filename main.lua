2ef6ff9e9ccfd27e40736104cef9e1ae1e076fde
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-01-07 16:52:59 +0100

    setup main.lua, call storyboard, send to start.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,22 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- DISCLAIMER -- CODE IS INTENDATED FOR ROLLING UP IN IDE 
 
--- Your code here
+-- Hide status bar
+
+	display.setStatusBar( display.HiddenStatusBar )
+
+-- Set anchor points - registration point of display - upper left side
+
+	display.setDefault( "anchorX", 0 )
+	display.setDefault( "anchorY", 0 )
+
+-- Random generator for code
+
+	math.randomseed( os.time( ) )
+
+-- Calling Storyboard library to our game
+
+	local storyboard = require "storyboard"
+
+-- Load start screen (it sends application to file 'start.lua')
+
+	storyboard.gotoScene( "start" )
