c508c7b6f6eb7beb9a06e7e74b87083380eb8542
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-01-13 21:15:21 +0100

    add variables, set required libraries

---
+++ gameover.lua
@@ -0,0 +1,14 @@
+
+-- initialize composer
+local composer = require( "composer" )
+local scene = composer.newScene( )
+
+-- variables
+local gameOverText
+local newGameButton
+
+-- required in module
+return scene
+
+
+--
