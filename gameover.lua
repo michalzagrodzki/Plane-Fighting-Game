4bf3979193261c571219635076aacccced1fb216
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-01-13 21:18:42 +0100

    correct names of sections, init create scene

--- gameover.lua
+++ gameover.lua
@@ -1,14 +1,21 @@
 
--- initialize composer
-local composer = require( "composer" )
-local scene = composer.newScene( )
+-- Call Composer
 
--- variables
-local gameOverText
-local newGameButton
+    local composer = require( "composer" )
+    local scene = composer.newScene( )
+
+-- Variables - elements of game
+
+    local gameOverText
+    local newGameButton
 
 -- required in module
-return scene
+
+    return scene
 
 
---
+-- Create 'game scene'
+
+    function scene:create( event )
+        -- body
+    end
